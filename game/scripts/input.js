1c6cac87835cfe32ab4d502a46097161b55f7983
TriremeSolutions <themellowread@gmail.com>
2023-09-13 17:40:27 +0100

    simulate keyboard input on touchscreen and attempt to detect and record via similar means until touch is over

--- game/scripts/input.js
+++ game/scripts/input.js
@@ -31,6 +31,52 @@ class InputHandler{
             }
             console.log(e.key, this.presskey);
         });
+
+        //simulate keyboard input
+
+        const touch_ev =(key, designation)=>{
+            console.log(key);
+            if ((key === designation)
+                && this.presskey.indexOf(key) === -1
+            ){ this.presskey.push(key) }
+
+            console.log(key, this.presskey);
+        }
+
+        const touch_ev_up =(key, designation)=>{
+                console.log(key);
+                if ((key === designation)){
+                    this.presskey.splice(this.presskey.indexOf(key), 1);
+                }
+            
+            console.log(key, this.presskey);
+        }
+
+        const simkey =(knob, designation)=> {
+            knob.addEventListener('touchstart', e => {
+                let key = e.changedTouches[0].target.textContent;
+                touch_ev(key, designation);
+                const duration = setInterval(()=>touch_ev(key, designation), 300);
+                sessionStorage.setItem('dur',duration);
+            });
+
+            knob.addEventListener('touchend', e => {
+                const duration = sessionStorage.getItem('dur');
+                clearInterval(duration);
+                let key = e.changedTouches[0].target.textContent;
+                touch_ev_up(key, designation);
+            });
+
+        }
+
+        simkey(up_k, "ArrowUp");
+        simkey(dw_k, "ArrowDown");
+        simkey(lf_k, "ArrowLeft");
+        simkey(rt_k, "ArrowRight");
+        simkey(w_k, "w");
+        simkey(a_k, "a");
+        simkey(s_k, "s");
+        simkey(d_k, "d");
     }
 }
 
